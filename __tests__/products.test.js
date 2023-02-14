8c7a8207e30a8560463a98e83b70762da3b68eff
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-02-14 17:40:52 +0300

    adds test for get /products/:productId

--- __tests__/products.test.js
+++ __tests__/products.test.js
@@ -3,18 +3,18 @@
 // On Windows you cannot use NODE_OPTIONS (and all env vars) from command line --> YOU HAVE TO USE CROSS-ENV PACKAGE TO BE ABLE TO PASS
 // ENV VARS TO COMMAND LINE SCRIPTS ON ALL OPERATIVE SYSTEMS!!!
 
-import supertest from "supertest"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import server from "../src/server.js"
-import ProductsModel from "../src/api/products/model.js"
+import supertest from "supertest";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import server from "../src/server.js";
+import ProductsModel from "../src/api/products/model.js";
 
-dotenv.config() // This command forces .env vars to be loaded into process.env. This is the way to do it whenever you can't use -r dotenv/config
+dotenv.config(); // This command forces .env vars to be loaded into process.env. This is the way to do it whenever you can't use -r dotenv/config
 
 // supertest is capable of executing server.listen of our Express app if we pass the Express server to it
 // It will give us back a client that can be used to run http requests on that server
 
-const client = supertest(server)
+const client = supertest(server);
 
 /* describe("Test APIs", () => {
   it("Should test that GET /test endpoint returns 200 and a body containing a message", async () => {
@@ -29,42 +29,68 @@ const validProduct = {
   name: "A valid product",
   description: "balllablalblabl",
   price: 100,
-}
+};
 
 const notValidProduct = {
   name: "A not valid product",
   price: 100,
-}
+};
+
+const notExistingId = "123456123456123456123456";
 
 beforeAll(async () => {
-  await mongoose.connect(process.env.MONGO_URL_TEST)
-  const product = new ProductsModel({ name: "test", description: "blalblabla", price: 20 })
-  await product.save()
-})
+  await mongoose.connect(process.env.MONGO_URL_TEST);
+  const product = new ProductsModel({
+    name: "test",
+    description: "blalblabla",
+    price: 20,
+  });
+  await product.save();
+});
 // beforeAll is a Jest hook ran before all the tests, usually it is used to connect to the db and to do some initial setup (like inserting some mock data in the db)
 
 afterAll(async () => {
-  await ProductsModel.deleteMany()
-  await mongoose.connection.close()
-})
+  await ProductsModel.deleteMany();
+  await mongoose.connection.close();
+});
 // afterAll hook could be used to clean up the situation (close the connection to Mongo gently and clean up db/collections)
 
 describe("Test APIs", () => {
   it("Should test that the env vars are set correctly", () => {
-    expect(process.env.MONGO_URL_TEST).toBeDefined()
-  })
+    expect(process.env.MONGO_URL_TEST).toBeDefined();
+  });
 
-  it("Should test that POST /products returns a valid _id and 201", async () => {
-    const response = await client.post("/products").send(validProduct).expect(201)
-    expect(response.body._id).toBeDefined()
-  })
-
+  // Fetching on /products/ should return a success status code and a body
   it("Should test that GET /products returns a success status and a body", async () => {
-    const response = await client.get("/products").expect(200)
-    console.log(response.body)
-  })
+    const response = await client.get("/products").expect(200);
+    expect(response.body).toBeDefined();
+    console.log(response.body);
+  });
+
+  // Create new product on /products should return a valid _id and 201 in case of a valid product, 400 if not
+  it("Should test that POST /products returns a valid _id and 201", async () => {
+    const response = await client
+      .post("/products")
+      .send(validProduct)
+      .expect(201);
+    expect(response.body._id).toBeDefined();
+  });
 
   it("Should test that POST /products with a not valid product returns a 400", async () => {
-    await client.post("/products").send(notValidProduct).expect(400)
-  })
-})
+    await client.post("/products").send(notValidProduct).expect(400);
+  });
+
+  // When retrieving the /products/:id endpoint:
+  // expect requests to be 404 with a non-existing id, like 123456123456123456123456. Use a 24 character ID or casting to ObjectID will fail
+  // expect requests to return the correct product with a valid id
+
+  it("Should test that GET /products/:productId with a not valid product returns a 404", async () => {
+    await client.get(`/products/${notExistingId}`).expect(404);
+  });
+
+  it("Should test that GET /products/:productId with a valid id", async () => {
+    const response = await client.post("/products").send(validProduct);
+    const existingId = response.body._id;
+    await client.get(`/products/${existingId}`).expect(200);
+  });
+});
--- src/api/products/index.js
+++ src/api/products/index.js
@@ -1,25 +1,44 @@
-import express from "express"
-import ProductsModel from "./model.js"
+import express from "express";
+import createHttpError from "http-errors";
+import ProductsModel from "./model.js";
 
-const productsRouter = express.Router()
+const productsRouter = express.Router();
 
 productsRouter.get("/", async (req, res, next) => {
   try {
-    const products = await ProductsModel.find()
-    res.send(products)
+    const products = await ProductsModel.find();
+    res.send(products);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 productsRouter.post("/", async (req, res, next) => {
   try {
-    const newProduct = new ProductsModel(req.body)
-    const { _id } = await newProduct.save()
-    res.status(201).send({ _id })
+    const newProduct = new ProductsModel(req.body);
+    const { _id } = await newProduct.save();
+    res.status(201).send({ _id });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default productsRouter
+productsRouter.get("/:productId", async (req, res, next) => {
+  try {
+    const product = await ProductsModel.findById(req.params.productId);
+    if (product) {
+      res.send(product);
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Product with id ${req.params.productId} not existing!`
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default productsRouter;
